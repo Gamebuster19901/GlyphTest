a2a6ded0832a68c68d1fdc20145c826fe8b88918
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2018-02-21 07:42:24 -0500

    push

---
+++ src/main/java/com/gamebuster19901/glyphTest/Main.java
@@ -0,0 +1,33 @@
+package com.gamebuster19901.glyphTest;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.CopyOption;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "glyph", name = "GlyphTest")
+public class Main {
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent e) {
+		//Grab Glyph Resources.zip from this jar and place it in the resource pack folder if it doesn't exist
+		try {
+			Path dest = new File(System.getProperty("user.dir") + "\\resourcepacks\\Glyph Resources.zip").toPath();
+			InputStream jarPack = getClass().getResourceAsStream("\\Glyph Resources.zip");
+			Files.copy(jarPack, dest);
+		} catch (IOException e1) {
+			if(e1 instanceof FileAlreadyExistsException){return;}
+			throw new IOError(e1);
+		}
+	}
+	
+}
